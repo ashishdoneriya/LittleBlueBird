6ac2f6182daef8d99f4d68343e466dc679037923
Brent Dunklau <bdunklau@yahoo.com>
2013-08-06 16:22:22 -0500

    break up controllers.js into fragments/js

---
+++ fragments/js/test.js
@@ -0,0 +1,7 @@
+
+  
+  
+  // 2013-08-03 created to test whatever you want temporarily
+  $scope.test = function(form) {
+  }
+  
--- fragments/js/user.js
+++ fragments/js/user.js
@@ -1,22 +1,4 @@
-// 2013-05-31: Site doesn't run in IE when the js debugger is off because (I think) console.log is undefined
-// This fixes that I hope.  See http://www.sitepoint.com/forums/showthread.php?575320-how-not-to-let-console-log%28%29-to-cause-error-on-IE-or-other-browsers
-var debugging = true;//false; // true sends console.log() stuff to the console. false means that stuff won't appear in the console
-if (typeof console == "undefined") var console = { log: function() {} };
-else if (!debugging || typeof console.log == "undefined") console.log = function() {};
 
-
-// 2013-07-23  $location is causing problems with jquery mobile: the browser back button stops working.  I think all links/routing stopped working.
-//function LbbController($scope, Email, $rootScope, User, $location) {
-
-// 2013-07-23  weird syntax needed for minification
-var LbbController = ['$scope', 'Email', '$rootScope', 'User', 'Gift', 'Password', 'FacebookUser', 'MergeUsers',
-function($scope, Email, $rootScope, User, Gift, Password, FacebookUser, MergeUsers) {
-
-  $scope.footermenu = '';
-  $scope.eventfilter = 'current';
-  $scope.email = 'bdunklau@yahoo.com';
-  $scope.username = 'bdunklau';
-  $scope.password = 'bdunklau';
   
   // copied/adapted from $scope.mergeaccount in app-UserCtrl.js 2013-08-03
   $scope.mergeaccount = function(user) {
@@ -358,12 +340,3 @@ function($scope, Email, $rootScope, User, Gift, Password, FacebookUser, MergeUse
     return $scope.footermenu == menuitem ? 'ui-btn-active ui-state-persist' : '';
   }
   
-  
-  
-  // 2013-08-03 created to test whatever you want temporarily
-  $scope.test = function(form) {
-  }
-  
-  
-}];
-

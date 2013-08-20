144a0d799abdf1e2122662e63c16e9f75e4c7f89
Brent Dunklau <bdunklau@yahoo.com>
2013-08-20 16:32:46 -0500

    -

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -847,25 +847,28 @@ function($scope, Email, $rootScope, User, Gift, Password, FacebookUser, MergeUse
 // see https://github.com/phonegap-build/BarcodeScanner/blob/master/README.md
 
   $scope.scan = function() {
-    alert('window.cordova='+window.cordova);
-    alert('window.PhoneGap='+window.PhoneGap);
   
-    var scanner = window.cordova.require("cordova/plugin/BarcodeScanner");
-    
-
-    scanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-    );
-    
-    $scope.scanner = JSON.stringify(scanner);
+    try {
+	    var scanner = window.cordova.require("cordova/plugin/BarcodeScanner");
+	    
+	
+	    scanner.scan(
+	      function (result) {
+	          alert("We got a barcode\n" +
+	                "Result: " + result.text + "\n" +
+	                "Format: " + result.format + "\n" +
+	                "Cancelled: " + result.cancelled);
+	      }, 
+	      function (error) {
+	          alert("Scanning failed: " + error);
+	      }
+	    );
+	    
+	    $scope.scanner = JSON.stringify(scanner);
+    }
+    catch(err) {
+      alert('Error:'+err.message);
+    }
   }
   
 }];
